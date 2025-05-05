8f2e26ae278d6af7ffd0113fc5760b084ede8239
catburger1230 <emil.r.wollf@gmail.com>
2025-05-05 09:55:02 +0200

    made sushi spawner

---
+++ Assets/Scripts/sushi_spawner.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sushi_spawner : MonoBehaviour
+{
+    public List<GameObject> allenemyes;
+    public GameObject sushi;
+    public Vector2 spawnpos = new Vector2(0, 0);
+    public bool spawn;
+    public int RandomNum;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spawnpos = sushi.transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawn)
+        {
+            sushi = gameObject.transform.GetChild(Random.Range(0, 2)).gameObject;
+            
+            GameObject Spawned_sushi = Instantiate(sushi, spawnpos, Quaternion.identity);
+            //allenemyes.Add(newPlatform);
+            Spawned_sushi.SetActive(true);
+            //newPlatform.GetComponent<enemye_script>();
+            spawn = false;
+        }
+
+    }
+}
