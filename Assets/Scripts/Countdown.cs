6d26d1b53286e7b8d58ec4f28191c28bd0a45a34
GOL64 <182638630+Dragehelt@users.noreply.github.com>
2025-05-15 16:19:38 +0200

    Finished Covayors, ingredients, and began on scores

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using TMPro;
 using UnityEngine;
@@ -5,17 +6,28 @@ using UnityEngine;
 public class Countdown : MonoBehaviour
 {
     public float RemainingTime = 60;
-    public TMP_Text messageText;
+    public TMP_Text scoreandtime;
+    public Boolean Morepoints;
+    private int points;
 
 
     private void Start()
     {
         Time.timeScale = 1f;
+        Morepoints = false;
+        points = 0;
     }
 
     void Update()
     {
         showCountdown();
+        countdowntimer();
+        updateScore(); 
+        
+    }
+
+    private void countdowntimer()
+    {
         if (RemainingTime > 0)
         {
             RemainingTime -= Time.deltaTime;
@@ -25,13 +37,20 @@ public class Countdown : MonoBehaviour
             //spawn_restartbotton
             Time.timeScale = 0f;
         }
-
     }
 
+    private void updateScore()
+    {
+        if (Morepoints == true)
+        {
+            Morepoints = false;
+            points += 100;
+        }
+    }
 
 
     private void showCountdown()
     {
-        messageText.SetText("Time: " + ((int)RemainingTime).ToString());
+        scoreandtime.SetText("Points: " + points.ToString() + "   Time: " + ((int)RemainingTime).ToString());
     }
 }
