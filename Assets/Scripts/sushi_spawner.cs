1c2992f3637927f4cb49b1ef4f76aa6fa144cc7b
catburger1230 <emil.r.wollf@gmail.com>
2025-05-18 21:28:24 +0200

    merge fix

--- Assets/Scripts/sushi_spawner.cs
+++ Assets/Scripts/sushi_spawner.cs
@@ -7,23 +7,17 @@ public class sushi_spawner : MonoBehaviour
     public GameObject sushi;
     public Vector3 spawnpos;
     public Vector3 distance;
-    private float num;
-    public float PauseForSpawn;
-    public bool spawn = false;
+    //public float PauseForSpawn;
+    //public bool spawn = false;
     private float timetilspawn = 1;
-    void Start()
-    {
-       
-    }
-
     void Update()
     {
-        num += 1*Time.deltaTime;
+        timetilspawn -= 1*Time.deltaTime;
 
-        if (num >= PauseForSpawn && PauseForSpawn != 0)
+        if (timetilspawn <= 0)
         {
-            num=0;
             Spawn();
+            timetilspawn = 1;
         }
     }
     public void Spawn()
@@ -32,29 +26,5 @@ public class sushi_spawner : MonoBehaviour
         GameObject Spawned_sushi = Instantiate(sushi, spawnpos, Quaternion.identity);
         Spawned_sushi.SetActive(true);
         //distance += new Vector3(-1, 0, 0);
-        countdowntimer();
-        if (spawn == true)
-        {
-            sushi = gameObject.transform.GetChild(Random.Range(0, gameObject.transform.childCount+1)).gameObject;
-            GameObject Spawned_sushi = Instantiate(sushi, spawnpos, Quaternion.identity);
-            Spawned_sushi.SetActive(true);
-            spawn = false;
-            timetilspawn = 1;
-            
-        }
-        
     }
-
-    private void countdowntimer()
-    {
-        if (timetilspawn > 0)
-        {
-            timetilspawn -= Time.deltaTime;
-        }
-        else if (timetilspawn <= 0)
-        {
-            spawn = true;
-        }
-    }
-
 }
